b999e744f79c3c39426c584512bc87454d4bcd86
Eric Wamari <wamari@harlertechnologies.com>
2018-05-10 12:57:27 +0300

    quiz7 ans

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -20,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        display(1);
+        display(77*2+1);
     }
 
     /**
